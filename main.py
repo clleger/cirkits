87ade2decffb4f79cb26a443c2b3150b84604cf7
Chris Leger <clleger@server.fake>
2016-09-21 23:54:42 -0600

    Added capability to dynamically load levels

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from kivy.config import Config
+Config.set('graphics', 'width', '960')
+Config.set('graphics', 'height', '640')
+
 from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 
 from cirkits.EditableLabel import EditableLabel
 import cirkits.Levels as Levels
-
-Config.set('graphics', 'width', '960')
-Config.set('graphics', 'height', '640')
 
 from kivy.app import App
 from kivy.uix.button import Button
@@ -26,15 +27,40 @@ from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 import boolean, Screens
 
 
-
+presentation = Builder.load_file("cirkitbuilder.kv")
 
 class CirkitGame(Widget):
     pass
 
 class CirkitBuilderApp(App):
+    def load_levels(self, pres):
+        menu = pres.get_screen('menu')
+        cirkit = pres.get_screen('cirkit')
+
+        levels = filter(lambda x: x.startswith('get_level_'), Levels.__dict__)
+
+        for func in levels:
+            def load_level(*args):
+                pres.transition.direction = 'left'
+                cirkit.clear_widgets()
+                Levels.__dict__[func](cirkit)
+                fl = FloatLayout()
+                back_button = Button(color=(0,1,0,1), font_size=25, size_hint=(0.3,0.2), text="Main Menu",
+                                     pos_hint={"right":1, "bottom":1})
+                def main_screen(*args):
+                    pres.transition.direction = 'right'
+                    pres.current = 'menu'
+                back_button.bind(on_release=main_screen)
+                fl.add_widget(back_button)
+                cirkit.add_widget(fl)
+                pres.current = 'cirkit'
+            lvl_name = func[len('get_level_'):]
+            lbutton = Button(text="Level %s" % lvl_name, font_size=50)
+            lbutton.bind(on_release=load_level)
+            menu.levels.add_widget(lbutton)
+
     def build(self):
-        presentation = Builder.load_file("cirkitbuilder.kv")
-        Levels.get_level_3(presentation.get_screen('cirkit'))
+        self.load_levels(presentation)
         # Clock.schedule_interval(game.update, 1.0 / 60.0)
         return presentation
 
